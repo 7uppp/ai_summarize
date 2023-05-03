44260fb8ebb05a5f950250884da734cc05df5ceb
7uppp <markwang9315@gmail.com>
2023-05-03 14:24:11 +1000

    add axios fetch data method

--- src/services/article.js
+++ src/services/article.js
@@ -26,3 +26,30 @@ export const articleApi = createApi({
 })
 
 export const { useLazyGetSummaryQuery } = articleApi
+
+// use axios to fetch data
+
+// import axios from 'axios';
+
+// const rapidApiKey = import.meta.env.VITE_RAPID_API_ARTICLE_KEY;
+// const baseUrl = 'https://article-extractor-and-summarizer.p.rapidapi.com/';
+
+// //
+// const axiosInstance = axios.create({
+//   baseURL: baseUrl,
+//   headers: {
+//     'X-RapidAPI-Key': rapidApiKey,
+//     'X-RapidAPI-Host': 'article-extractor-and-summarizer.p.rapidapi.com',
+//   },
+// });
+
+// //
+// export const getArticleSummary = async (articleUrl) => {
+//   try {
+//     const encodedUrl = encodeURIComponent(articleUrl);
+//     const response = await axiosInstance.get(`summarize?url=${encodedUrl}&length=3`);
+//     return response.data;
+//   } catch (error) {
+//     console.error('Error fetching article summary:', error);
+//   }
+// };
